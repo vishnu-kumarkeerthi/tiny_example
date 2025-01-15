cf641f113ea80a7fdeacf855b1ed74753e0730b9
vishnu-kumarkeerthi <sivanagavishnukumar@gmail.com>
2025-01-15 12:37:18 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -20,8 +20,8 @@ input wire rst_n
 // The design simply swaps the high nibble [7:4] with the low nibble [3:0].
 assign uo_out = {ui_in[3:0], ui_in[7:4]};
 // Unused I/O signals are tied to 0 or set as inputs only.
-assign uio_out = 8'b0000_0000;
-assign uio_oe = 8'b0000_0000;
+assign uio_out = 8'b00000000;
+assign uio_oe = 8'b00000000;
 // Tie off unused signals to prevent warnings.
 wire _unused = &{ena, clk, rst_n, uio_in, 1’b0};
 endmodule
