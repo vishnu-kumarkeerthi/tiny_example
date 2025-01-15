003633c5bdcde4652e326442e0b29c0ab840024d
vishnu-kumarkeerthi <sivanagavishnukumar@gmail.com>
2025-01-15 14:07:39 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -19,8 +19,8 @@ wire [7:0] uio_out;
 wire [7:0] uio_oe;
    
 `ifdef GL_TEST
-   wire VPWR = 1’b1;
-   wire VGND = 1’b0;
+   wire VPWR = 1'b1;
+   wire VGND = 1'b0;
 `endif
    tt_um_example user_project( 
 
