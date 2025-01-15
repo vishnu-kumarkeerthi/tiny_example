372211525fd33f1e6806257cd29680e2c95821f8
vishnu-kumarkeerthi <sivanagavishnukumar@gmail.com>
2025-01-15 11:58:47 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-‘default_nettype none
+`default_nettype none
+
 module tt_um_example (
 input wire [7:0] ui_in, // Dedicated inputs (8 bits)
 output wire [7:0] uo_out, // Dedicated outputs (8 bits)
@@ -19,9 +20,10 @@ input wire rst_n
 // The design simply swaps the high nibble [7:4] with the low nibble [3:0].
 assign uo_out = {ui_in[3:0], ui_in[7:4]};
 // Unused I/O signals are tied to 0 or set as inputs only.
-assign uio_out = 8’b0000_0000;
-assign uio_oe = 8’b0000_0000;
+assign uio_out = 8'b0000_0000;
+assign uio_oe = 8'b0000_0000;
 // Tie off unused signals to prevent warnings.
 wire _unused = &{ena, clk, rst_n, uio_in, 1’b0};
 endmodule
-‘default_nettype none
+
+`default_nettype none
